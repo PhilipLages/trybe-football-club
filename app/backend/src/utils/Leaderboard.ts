25ec5b2ee1deb5c32dc7d10023ecc69444a55b7b
Philip Lages <psilvaelages@gmail.com>
2023-01-26 11:18:57 -0300

    feat: finishes class Leaderboard, now it is possible to create a full leaderboard

--- app/backend/src/utils/Leaderboard.ts
+++ app/backend/src/utils/Leaderboard.ts
@@ -9,29 +9,29 @@ export default class Leaderboard {
     this._where = where;
   }
 
-  private _calculateHomeTeamPoints = ({ homeMatches }: IMatchesByTeam) => homeMatches
+  private calculateHomeTeamPoints = ({ homeMatches }: IMatchesByTeam) => homeMatches
     .map(({ homeTeamGoals, awayTeamGoals }) => {
       if (homeTeamGoals > awayTeamGoals) return 3;
 
       return homeTeamGoals === awayTeamGoals ? 1 : 0;
     }).reduce((acc: number, curr: number) => acc + curr, 0);
 
-  private _calculateAwayTeamPoints = ({ awayMatches }: IMatchesByTeam) => awayMatches
+  private calculateAwayTeamPoints = ({ awayMatches }: IMatchesByTeam) => awayMatches
     .map(({ homeTeamGoals, awayTeamGoals }) => {
       if (homeTeamGoals < awayTeamGoals) return 3;
 
       return homeTeamGoals === awayTeamGoals ? 1 : 0;
     }).reduce((acc: number, curr: number) => acc + curr, 0);
 
-  private _getPoints = (match: IMatchesByTeam) => {
+  private getPoints = (match: IMatchesByTeam) => {
     if (this._where === 'home') {
-      return this._calculateHomeTeamPoints(match);
+      return this.calculateHomeTeamPoints(match);
     } if (this._where === 'away') {
-      return this._calculateAwayTeamPoints(match);
+      return this.calculateAwayTeamPoints(match);
     }
   };
 
-  private _getGames = ({ homeMatches, awayMatches }: IMatchesByTeam) => {
+  private getGames = ({ homeMatches, awayMatches }: IMatchesByTeam) => {
     if (this._where === 'home') {
       return homeMatches.length;
     } if (this._where === 'away') {
@@ -39,7 +39,7 @@ export default class Leaderboard {
     }
   };
 
-  private _getVictories = ({ homeMatches, awayMatches }: IMatchesByTeam) => {
+  private getVictories = ({ homeMatches, awayMatches }: IMatchesByTeam) => {
     if (this._where === 'home') {
       return homeMatches.filter((match) => match.homeTeamGoals > match.awayTeamGoals).length;
     } if (this._where === 'away') {
@@ -47,12 +47,74 @@ export default class Leaderboard {
     }
   };
 
+  private getDraws = ({ homeMatches, awayMatches }: IMatchesByTeam) => {
+    if (this._where === 'home') {
+      return homeMatches.filter((match) => match.homeTeamGoals === match.awayTeamGoals).length;
+    } if (this._where === 'away') {
+      return awayMatches.filter((match) => match.homeTeamGoals === match.awayTeamGoals).length;
+    }
+  };
+
+  private getLosses = ({ homeMatches, awayMatches }: IMatchesByTeam) => {
+    if (this._where === 'home') {
+      return homeMatches.filter((match) => match.homeTeamGoals < match.awayTeamGoals).length;
+    } if (this._where === 'away') {
+      return awayMatches.filter((match) => match.homeTeamGoals > match.awayTeamGoals).length;
+    }
+  };
+
+  private getGoalsFavor = ({ homeMatches, awayMatches }: IMatchesByTeam) => {
+    if (this._where === 'home') {
+      return homeMatches.map(({ homeTeamGoals }) => homeTeamGoals)
+        .reduce((acc: number, curr: number) => acc + curr, 0);
+    } if (this._where === 'away') {
+      return awayMatches.map(({ awayTeamGoals }) => awayTeamGoals)
+        .reduce((acc: number, curr: number) => acc + curr, 0);
+    }
+  };
+
+  private getGoalsOwn = ({ homeMatches, awayMatches }: IMatchesByTeam) => {
+    if (this._where === 'home') {
+      return homeMatches.map(({ awayTeamGoals }) => awayTeamGoals)
+        .reduce((acc: number, curr: number) => acc + curr, 0);
+    } if (this._where === 'away') {
+      return awayMatches.map(({ homeTeamGoals }) => homeTeamGoals)
+        .reduce((acc: number, curr: number) => acc + curr, 0);
+    }
+  };
+
+  private getBalance = (match: IMatchesByTeam) => {
+    const goalsFavor = this.getGoalsFavor(match);
+    const goalsOwn = this.getGoalsOwn(match);
+
+    if (!goalsFavor || !goalsOwn) return undefined;
+
+    return goalsFavor - goalsOwn;
+  };
+
+  private getEfficiency = (game: IMatchesByTeam) => {
+    const points = this.getPoints(game);
+    const games = this.getGames(game);
+
+    if (!points || !games) return undefined;
+
+    const efficiency = ((points / (games * 3)) * 100).toFixed(2);
+
+    return efficiency;
+  };
+
   public createLeaderboard = () => this._matches.map((match) => (
     {
       name: match.teamName,
-      totalPoints: this._getPoints(match),
-      totalGames: this._getGames(match),
-      totalVictories: this._getVictories(match),
+      totalPoints: this.getPoints(match),
+      totalGames: this.getGames(match),
+      totalVictories: this.getVictories(match),
+      totalDraws: this.getDraws(match),
+      totalLosses: this.getLosses(match),
+      goalsFavor: this.getGoalsFavor(match),
+      goalsOwn: this.getGoalsOwn(match),
+      goalsBalance: this.getBalance(match),
+      efficiency: this.getEfficiency(match),
     }
   ));
 }
