41a7f07449b2289320a9092b510b99ad286c728e
Philip Lages <psilvaelages@gmail.com>
2023-01-26 12:25:00 -0300

    feat: creates route to get away leaderboard

--- app/backend/src/router/leaderboardRouter.ts
+++ app/backend/src/router/leaderboardRouter.ts
@@ -6,5 +6,6 @@ const leaderboardRouter = express.Router();
 const leaderboardController = new LeaderboardController();
 
 leaderboardRouter.get('/home', leaderboardController.getHomeLeaderboard);
+leaderboardRouter.get('/away', leaderboardController.getAwayLeaderboard);
 
 export default leaderboardRouter;
